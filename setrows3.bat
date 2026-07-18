0438de2af0f4b7a129b4d2228a38ff1b8ae967cf
Joerg von Roos <40567703+jvr-ks@users.noreply.github.com>
2026-07-18 18:00:54 +0200

    0.017

---
+++ setrows3.bat
@@ -0,0 +1,25 @@
+@rem setrows3.bat
+
+@set rows=3
+
+@echo off
+
+cd %~dp0
+
+net session >nul 2>&1
+if NOT %ERRORLEVEL% == 0 goto noadmin
+
+call healtaskbarrows.exe rows=%rows% noshutdown
+goto end
+
+:noadmin
+echo Error, please run the script as an administrator!
+echo.
+pause
+goto end
+
+
+:end
+
+
+
